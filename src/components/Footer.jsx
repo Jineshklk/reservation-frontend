34e2632b992c56ec3b236d01394c31b758295f3e
Your Name <your.email@example.com>
2025-10-23 16:27:06 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">BookMyTable</div>
+          <div className="right">
+            <p>Available in Delhi,Bangalore and Noida</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed with Love❤</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
